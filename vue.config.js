4e28558f92d5a34aec3d5827b30c818f6460af59
HugoAtGithub <jorcion@gmail.com>
2020-11-27 16:08:51 +0800

    解决了 SVG 自带 fill 属性,导致图标不能变色的问题

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable */
 const path = require('path')
 
 module.exports = {
@@ -10,15 +10,16 @@ module.exports = {
       .test(/\.svg$/)
       .include.add(dir)
       .end()
-      .use('svg-sprite-loader')
-      .loader('svg-sprite-loader')
+      .use('svg-sprite-loader').loader('svg-sprite-loader')
       .options({
         extract: false,
         symbolId: 'icon-[name]'
       })
       .end()
+      .use('svgo-loader').loader('svgo-loader')
+      .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]}))
+      .end()
     config.plugin('svg-sprite')
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
       .use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
     config.module
       .rule('svg')
