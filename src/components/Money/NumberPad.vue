db192eeee70861a481d68bdb687a894e61c4f73a
HugoAtGithub <jorcion@gmail.com>
2020-11-30 00:11:19 +0800

    基本完成 numberPad 组件

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="numberPad">
-    <div class="output">1001</div>
-    <div class="buttons">
+    <div class="output">{{ output }}</div>
+    <div class="buttons" @click="inputContent">
       <button>1</button>
       <button>2</button>
       <button>3</button>
-      <button>删除</button>
+      <button @click.stop="remove">删除</button>
       <button>4</button>
       <button>5</button>
       <button>6</button>
-      <button>清空</button>
+      <button @click.stop="clear">清空</button>
       <button>7</button>
       <button>8</button>
       <button>9</button>
-      <button class="ok">OK</button>
+      <button class="ok" @click.stop="ok">OK</button>
       <button class="zero">0</button>
       <button>.</button>
     </div>
@@ -21,16 +21,62 @@
 </template>
 
 <script lang="ts">
-export default {
-  name: 'NumberPad'
-};
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class NumberPad extends Vue {
+  output = '0';
+
+  inputContent(event: MouseEvent) {
+    const button = (event.target as HTMLButtonElement);
+    const input = button.textContent ?? '0';
+    this.handleDigit(input);
+  }
+
+  private handleDigit(input: string) {
+    if (this.output.length === 16) {
+      return;
+    }
+    if (this.output === '0') {
+      if ('0123456789'.indexOf(input) >= 0) {
+        this.output = input;
+      } else {
+        this.output += input;
+      }
+      return;
+    }
+    if (this.output.indexOf('.') >= 0 && input === '.') {
+      return;
+    }
+    this.output += input;
+  }
+
+  remove() {
+    if (this.output.length === 1) {
+      this.output = '0';
+    } else {
+      this.output = this.output.slice(0, -1);
+    }
+  }
+
+  clear() {
+    this.output = '0';
+  }
+
+  ok() {
+    console.log('ok!');
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
+
 .numberPad {
   .output {
     @extend %innerShadow;
+    height: 72px;
     font-size: 36px;
     font-family: $font-mono;
     display: flex;
