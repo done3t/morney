1f13680f31515e0cb1a9bd7f315f92f5c999d4fd
HugoAtGithub <jorcion@gmail.com>
2020-11-30 18:20:10 +0800

    收集四个组件的数据到 record

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,27 +1,40 @@
 <template>
   <Layout class-prefix="layout">
-    <Tags :data-source.sync="tags"/>
-    <Notes/>
-    <Types/>
-    <NumberPad/>
+    {{ record }}
+    <Tags :data-source.sync="tags" :value.sync="record.tags"/>
+    <Notes :value.sync="record.notes"/>
+    <Types :type.sync="record.type"/>
+    <NumberPad :value.sync="record.amount"/>
   </Layout>
 </template>
 
 <script lang="ts">
+import Vue from 'vue';
 import Tags from '@/components/Money/Tags.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Types from '@/components/Money/Types.vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
+import {Component} from 'vue-property-decorator';
 
-export default {
-  name: 'Money',
+type Record = {
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number;
+}
+@Component({
   components: {NumberPad, Types, Notes, Tags},
-  data() {
-    return {
-      tags: ['衣', '食', '住', '行', '旅游']
-    };
-  }
-};
+
+})
+export default class Money extends Vue {
+  tags = ['衣', '食', '住', '行', '旅游'];
+  record: Record = {
+    tags: [],
+    notes: '',
+    type: '-',
+    amount: 0
+  };
+}
 </script>
 <style lang="scss">
 .layout-content {
