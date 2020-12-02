fb927ed8188760eb2a5ca81a734347723604596e
HugoAtGithub <jorcion@gmail.com>
2020-12-02 14:59:34 +0800

    封装 model： tagList recordList

---
+++ src/models/recordListModel.ts
@@ -0,0 +1,13 @@
+const localStorageKeyName = 'recordList';
+const recordListModel = {
+  clone(data: RecordItem[] | RecordItem) {
+    return JSON.parse(JSON.stringify(data));
+  },
+  fetch() {
+    return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
+  },
+  save(data: RecordItem[]) {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+  }
+};
+export default recordListModel;
