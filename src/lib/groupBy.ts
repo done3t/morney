3fac90e1e4866877f6053a208e4db0af1038beeb
HugoAtGithub <jorcion@gmail.com>
2020-12-10 19:22:41 +0800

    完成统计页面

---
+++ src/lib/groupBy.ts
@@ -0,0 +1,15 @@
+function groupBy<T>(list: Array<T>, keyGetter: Function) {
+  const map = new Map();
+  list.forEach((item) => {
+    const key = keyGetter(item);
+    const collection = map.get(key);
+    if (!collection) {
+      map.set(key, [item]);
+    } else {
+      collection.push(item);
+    }
+  });
+  return map;
+}
+
+export default groupBy;
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,10 +1,11 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
     <ol>
-      <li v-for="(group, index) in result" :key="index">
-        <h3 class="title">{{ group.title }}</h3>
+      <li v-for="(group, index) in groupList" :key="index">
+        <h3 class="title">
+          <span>{{ beautify(group.title) }}</span>
+          <span>￥{{ group.total }}</span></h3>
         <ol>
           <li v-for="item in group.items" :key="item.id"
               class="record">
@@ -24,6 +25,9 @@ import Tabs from '@/components/Tabs.vue';
 import {Component} from 'vue-property-decorator';
 import intervalList from '@/constants/intervalList';
 import recordTypeList from '@/constants/recordTypeList';
+import dayjs from 'dayjs';
+import clone from '@/lib/clone';
+import groupBy from '@/lib/groupBy';
 
 @Component({
   components: {Tabs}
@@ -39,25 +43,51 @@ export default class Statistics extends Vue {
     return tags.length === 0 ? '无' : tags.join(',');
   }
 
+  beautify(dateString: string) {
+    const day = dayjs(dateString);
+    const now = dayjs();
+    const diffDay = day.diff(now, 'day');
+    if (diffDay === 0) {
+      return '今天';
+    } else if (diffDay === -1) {
+      return '昨天';
+    } else if (diffDay === -2) {
+      return '前天';
+    } else if (day.isSame(now, 'year')) {
+      return day.format('M月D日');
+    } else {
+      return day.format('YYYY年M月D日');
+    }
+  }
+
   get recordList() {
     return (this.$store.state as RootState).recordList;
   }
 
-  get result() {
+  get groupList() {
     const {recordList} = this;
-    type HashTableValue = {
-      title: string;
-      items: RecordList[];
+    if (recordList.length === 0) {
+      return [];
     }
-    const hashTable: { [key: string]: HashTableValue } = {};
-    for (const recordItem of recordList) {
-      const date = recordItem.createdAt?.split('T')[0];
-      hashTable[date] = hashTable[date] ?? {title: date, items: []};
-      hashTable[date].items.push(recordItem);
-    }
-    return hashTable;
-  }
+    const newList = clone(recordList)
+        .filter(item => item.type === this.type)
+        .sort((a, b) => dayjs(b).diff(dayjs(a)));
 
+    type Result = {
+      title: string;
+      total?: number;
+      items: RecordItem[];
+    }[]
+    const result: Result = [];
+    const groupMap: Map<string, RecordItem[]> = groupBy(newList,
+        item => dayjs(item.createdAt).format('YYYY-MM-DD'));
+    groupMap.forEach((value, key) => {
+      const resultItem = {title: key, items: value};
+      resultItem.total = value.reduce((sum, item) => sum += item.amount, 0);
+      result.push(resultItem);
+    });
+    return result;
+  }
 
   beforeCreate() {
     this.$store.commit('fetchRecords');
