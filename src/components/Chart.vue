ffe9f1cf934a9760c50969d42778b97183f12cb3
HugoAtGithub <jorcion@gmail.com>
2020-12-15 17:18:46 +0800

    封装echarts组件

---
+++ src/components/Chart.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper" ref="wrapper"></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component, Prop, Watch} from 'vue-property-decorator';
+import {EChartOption, ECharts, init} from 'echarts';
+
+@Component
+export default class Chart extends Vue {
+  @Prop() options?: EChartsOption;
+  chart?: ECharts;
+
+  mounted() {
+    if (this.options === undefined) {
+      return console.error('options 为空');
+    }
+    this.chart = init(this.$refs.wrapper as HTMLDivElement);
+    this.chart.setOption(this.options);
+  }
+
+  @Watch('options')
+  noOptionsChange(newValue: EChartOption) {
+    this.chart?.setOption(newValue);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  height: 400px;
+}
+</style>
